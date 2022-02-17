916cef6e843a558f9a2e9f120a514a5cf58716cd
Serge018 <sivenskij@gmail.com>
2022-02-17 03:53:55 +0300

    Добавлен класс Course

---
+++ src/Lesson_01/Course.java
@@ -0,0 +1,13 @@
+package Lesson_01;
+
+public class Course {
+    int[] distanses = { 122, 75, 32, 45, 20 };
+
+    public boolean doIt(Team team) {
+        for (int distanse : distanses) {
+            team.run(distanse);
+        }
+
+        return true;
+    }
+}
