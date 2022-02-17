bc708983060f5fd015e2b2c42b0b97c8647a30bc
Serge018 <sivenskij@gmail.com>
2022-02-17 04:09:23 +0300

    Исправлены отступы между методами

--- src/Lesson_01/Course.java
+++ src/Lesson_01/Course.java
@@ -3,6 +3,7 @@ package Lesson_01;
 public class Course {
     int[] distanses = { 122, 75, 32, 45, 20 };
 
+
     public boolean doIt(Team team) {
         for (int distanse : distanses) {
             team.run(distanse);
--- src/Lesson_01/Player.java
+++ src/Lesson_01/Player.java
@@ -1,14 +1,17 @@
 package Lesson_01;
 
+
 public class Player implements ISportsman {
     private String name;
     private int speed;
     private float totalTime;
 
+
     public Player(String name, int speed) {
         this.name = name;
         this.speed = speed;
     }
+
 
     public float run(int distance) {
         float time = Float.valueOf(distance) / Float.valueOf(speed);
@@ -16,6 +19,7 @@ public class Player implements ISportsman {
 
         return time;
     }
+
 
     public String getResult() {
         String result = name + " - " + String.format("%.2f", totalTime);
