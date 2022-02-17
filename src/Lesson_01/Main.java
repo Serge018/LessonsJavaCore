ded4753b72479521f2cd41a5bf905c85746fd554
Serge018 <sivenskij@gmail.com>
2022-02-17 03:50:05 +0300

    Реализован метод main() класса Main

--- src/Lesson_01/Main.java
+++ src/Lesson_01/Main.java
@@ -3,6 +3,22 @@ package Lesson_01;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Создаем полосу препятствий
+        Course course = new Course();
+
+        // Создаем команду
+        Player[] players = {
+            new Player("Raphael", 7),
+            new Player("Leonardo", 6),
+            new Player("Donatello", 8),
+            new Player("Michelangelo", 9)
+        };
+        Team team = new Team("TMNT", players);
+
+        // Просим команду пройти полосу
+        course.doIt(team);
+
+        // Показываем результаты
+         team.showResults();
     }
 }
