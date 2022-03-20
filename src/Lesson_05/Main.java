5be9c709878e1f71938235258cdaeacc7e617c0e
Serge018 <sivenskij@gmail.com>
2022-03-21 00:40:51 +0300

    Сделано задание №1

--- src/Lesson_05/Main.java
+++ src/Lesson_05/Main.java
@@ -1,14 +1,26 @@
 package Lesson_05;
 
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static final String FILE_NAME = "table.csv";
-    static String[] headers = new String[] { "Заговок №1", "Заговок №3", "Заговок №3" };
+    static String[] headers = new String[] { "Title 01", "Title 02", "Title 03" };
 
     public static void main(String[] args) {
-        // Сохранение данных в файл table.csv
+        // Задание №1. Сохранение данных в файл table.csv
+        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+            // Генерируем таблицу с данными
+            String appData = newData().toString();
+            byte[] csvData = appData.getBytes(StandardCharsets.UTF_8);
 
-        AppData data = newData();
-        System.out.println(data);
+            // Записываем данные таблицы в файл table.csv
+            fileOutputStream.write(csvData);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 
     public static AppData newData() {
