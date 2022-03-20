0631d124910200e1f0373c2525ca519a0d562757
Serge018 <sivenskij@gmail.com>
2022-03-21 00:12:08 +0300

    В классе AppData реализован метод init()

--- src/Lesson_05/AppData.java
+++ src/Lesson_05/AppData.java
@@ -9,6 +9,13 @@ public class AppData {
     private Random random = new Random();
 
     public void init(String[] headers) {
+        this.headers = headers;
+        table = new int[headers.length][headers.length];
 
+        for (int row = 0; row < headers.length; row++) {
+            for (int column = 0; column < headers.length; column++) {
+                table[row][column]  = random.nextInt(10);
+            }
+        }
     }
 }
