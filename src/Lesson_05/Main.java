9f73658ae8703d53919253280591d9a2c351b771
Serge018 <sivenskij@gmail.com>
2022-03-21 01:11:17 +0300

    Реализовано решение задачи №2
    Добавлен метод printFileContent()

--- src/Lesson_05/Main.java
+++ src/Lesson_05/Main.java
@@ -2,18 +2,21 @@ package Lesson_05;
 
 import java.io.IOException;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static final String FILE_NAME = "table.csv";
     static String[] headers = new String[] { "Title 01", "Title 02", "Title 03" };
 
+
     public static void main(String[] args) {
         // Задание №1. Сохранение данных в файл table.csv
         AppData appData = newData();
         save(appData);
 
-        // Задание №2.
+        // Задание №2. Чтение файла table.csv
+        printFileContent();
     }
 
 
@@ -33,6 +36,21 @@ public class Main {
             String text = appData.toString();
             byte[] data = text.getBytes(StandardCharsets.UTF_8);
             fileOutputStream.write(data);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+    /*
+    * Выводит на экран данные файла table.csv
+    */
+    public static void printFileContent() {
+        try (FileInputStream fileInputStream = new FileInputStream(FILE_NAME)) {
+            byte[] data = new byte[fileInputStream.available()];
+            fileInputStream.read(data);
+            String table = new String(data);
+            System.out.println(table);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
